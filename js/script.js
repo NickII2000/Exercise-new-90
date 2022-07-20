bddf637ddfd914fda419fcb8d6d4b22f86ccb202
Nick <Nick_II2000@mail.ru>
2022-07-20 07:53:53 +0600

    2-th commit

--- js/script.js
+++ js/script.js
@@ -323,34 +323,12 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 
-    // fetch('https://jsonplaceholder.typicode.com/todos/1')
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
-
-    // fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
-
-    // fetch('https:/jsonplaceholder.typicode.com/posts', {
-    //     method: "POST",
-    //     body: JSON.stringify({ name: 'Nick' }),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // })
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
-
     // json - server(Exercise 89)
 
     // fetch('db.json')
+    // fetch('http://localhost:3000/requests')
     fetch('http://localhost:3000/menu')
-        // fetch('http://localhost:3000/requests')
         .then(data => data.json())
         .then(res => console.log(res));
 
-    // fetch('http://localhost:3000/menu')
-    fetch('http://localhost:3000/requests')
-        .then(data => data.json())
-        .then(res => console.log(res));
 });
